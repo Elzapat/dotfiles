9fc18d9505cb334c6b06e05db6c4761a083bb214
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2021-10-20 18:52:32 +0200

    moved plugins.lua and added plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
-local packer = require("packer")
+require("plugins")
 
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
@@ -6,8 +6,8 @@ vim.opt.background = "dark"
 vim.opt.mouse = "a"
 
 vim.cmd("colorscheme gruvbox")
-vim.api.nvim_set_keymap("n", "<C-h>", ":bprevious<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-l>", ":bnext<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-j>", ":bprevious<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-m>", ":bnext<CR>", { noremap = true })
 
 vim.cmd("highlight Normal guibg=none")
 vim.cmd("highlight Folded guibg=none")
@@ -21,33 +21,15 @@ let g:airline#extensions#tabline#fnamemod = ':t'
 let g:airline#extensions#tabline#formatter = 'unique_tail'
 let g:airline#extensions#nerdtree_statusline = 1
 let g:airline_powerline_fonts = 1
+
+noremap m l
+noremap l k
+noremap k j
+noremap j h
 ]], true)
-
-packer.startup(function()
-	use "wbthomason/packer.nvim"
-
-	use "ishan9299/nvim-solarized-lua"
-
-	use "sheerun/vim-polyglot"
-
-	use "neovim/nvim-lspconfig"
-
-	use "kyazdani42/nvim-web-devicons"
-
-	use "akinsho/bufferline.nvim"
-
-	use "windwp/nvim-autopairs"
-
-	use "vim-airline/vim-airline"
-	use "vim-airline/vim-airline-themes"
-
-	use "lukas-reineke/indent-blankline.nvim"
-
-	use { "ellisonleao/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } }
-end)
 
 require("bufferline").setup()
 require("nvim-autopairs").setup()
 -- require("plugins.indent_blankline")
-local plugins = require("plugins")
+--
 require("plugins.indent_blankline")
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,9 +1,27 @@
-vim.cmd [[ packadd packer.nvim ]]
-
 return require("packer").startup(function(use)
-	use "wbthomason/packet.nvim"
+	use "wbthomason/packer.nvim"
 
 	use "ishan9299/nvim-solarized-lua"
 
 	use "sheerun/vim-polyglot"
+
+	use "neovim/nvim-lspconfig"
+
+	use "kyazdani42/nvim-web-devicons"
+
+	use "akinsho/bufferline.nvim"
+
+	use "windwp/nvim-autopairs"
+
+	use "vim-airline/vim-airline"
+	use "vim-airline/vim-airline-themes"
+
+	use "lukas-reineke/indent-blankline.nvim"
+
+	use { "ellisonleao/gruvbox.nvim", requires = { "rktjmp/lush.nvim" } }
+
+	use {
+		"OmniSharp/omnisharp-vim",
+		ft = { "cs" }
+	}
 end)
