c08d5c544d63dd56271b7d28ca803aedb7e4b3e6
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2021-10-22 16:36:20 +0200

    added options and mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,13 +1,20 @@
 -- Misc
 vim.g.mapleader = "<Space>"
-vim.api.nvim_set_keymap("n", "<C-r>", ":source ~/.config/nvim/init.lua<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-r>", ":source ~/.config/nvim/init.lua<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>r", ":source ~/.config/nvim/init.lua<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>q", "<C-w><C-q>", { noremap = true })
 
 -- Edition
 vim.api.nvim_set_keymap("n", "K", "J", { noremap = true })
 
 -- Buffer movement
-vim.api.nvim_set_keymap("n", "<C-j>", ":bprevious<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-m>", ":bnext<CR>", { noremap = true })
+vim.api.nvim_set_keymap("", "<C-j>", ":bprevious<cr>", { noremap = true })
+vim.api.nvim_set_keymap("", "<C-m>", ":bnext<cr>", { noremap = true })
+
+vim.api.nvim_set_keymap("", "<leader>m", "<C-w>l", { noremap = true })
+vim.api.nvim_set_keymap("", "<leader>l", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("", "<leader>k", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("", "<leader>j", "<C-w>h", { noremap = true })
 
 -- Movement
 vim.api.nvim_set_keymap("", "m", "l", { noremap = true })
@@ -16,5 +23,7 @@ vim.api.nvim_set_keymap("", "k", "j", { noremap = true })
 vim.api.nvim_set_keymap("", "j", "h", { noremap = true })
 vim.api.nvim_set_keymap("", "h", "<Nop>", { noremap = true })
 
+vim.api.nvim_set_keymap("", "<leader>x", ":bd<cr>:bd #<cr>", { noremap = true })
+
 -- Telescope
-vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<cr>", { noremap = true })
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,4 +1,23 @@
 vim.opt.termguicolors = true
-vim.opt.relativenumber = true
 vim.opt.background = "dark"
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
 vim.opt.mouse = "a"
+
+vim.opt.incsearch = true
+vim.opt.nohlsearch = true
+
+vim.opt.smartcase = true
+
+vim.opt.lazyredraw = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.clipboard = vim.opt.clipboard + "unnamedplus"
+
+vim.opt.ttimeoutlet = true
+vim.opt.pumblend = true
+
+vim.opt.encoding = "UTF-8"
