93c3f9672547882b12fb23fc34bc97de7ff4df1d
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2021-11-21 19:45:47 +0100

    completely changed status bar to polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Terminate already running bar instances
 killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload -q main -c "~/.config/polybar/config.ini" &
+  done
+else
+  polybar --reload example &
+fi
 
-polybar mybar &
-
-echo "Polybar launched"
+echo "Bars launched..."
