e64f07ec703635184a0a669c216a764041ba5692
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-01-03 17:59:56 +0100

    other font sizes for desktop

--- .scripts/choose_random_font.sh
+++ .scripts/choose_random_font.sh
@@ -1,5 +1,3 @@
-_unused_fonts=()
-
 fonts=(
   # "CozetteVector:9.5"
   # "SauceCodePro Nerd Font:8.5"
@@ -10,7 +8,22 @@ fonts=(
   # "TerminessTTF Nerd Font:9.5"
 )
 
+if [ "$HOSTNAME" = "morgan-desktop" ]; then
+  fonts=(
+    # "CozetteVector:9.5"
+    # "SauceCodePro Nerd Font:8.5"
+    "JetBrainsMono Nerd Font:13.0"
+    "Hasklug Nerd Font:12.5"
+    "Iosevka:12.5"
+    "VictorMono Nerd Font:13.0"
+    # "TerminessTTF Nerd Font:9.5"
+  )
+fi
+
 random_index=$(($RANDOM % ${#fonts[@]}))
-sed -i "s/family: .*/family: ${fonts[$random_index]%%:*}/g" $HOME/.config/alacritty/alacritty.yml
-sed -i "s/size: .*/size: ${fonts[$random_index]##*:}/g" $HOME/.config/alacritty/alacritty.yml
+font=${fonts[$random_index]%%:*}
+size=${fonts[$random_index]##*:}
+
+sed -i "s/family: .*/family: $font/g" $HOME/.config/alacritty/alacritty.yml
+sed -i "s/size: .*/size: $size/g" $HOME/.config/alacritty/alacritty.yml
 touch $HOME/.config/alacritty/alacritty.yml
